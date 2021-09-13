82f633f9957ac609b497c6611d4b376c34a80b48
Mik3Monzon <mmonzonjr@gmail.com>
2021-09-13 13:07:31 -0500

    Clase 13/09/2021

--- Tarea2.py
+++ Tarea2.py
@@ -13,19 +13,16 @@ def sumaMayor(L):
         suma2 = 0
         while(tmp <= len(L)):
             suma = 0
-            copia = []
             for i in range(tmp, len(L)):
                 suma += L[i]
-                copia.append(L[i])
                 if(suma > suma2):
-                    copia2 = copia.copy()
                     suma2 = suma
             tmp = tmp+1
-        print("La secuencia de numero que da la suma mayor es: " + str(copia2))
         print('La mayor suma posible es ' + str(suma2))
 
 if __name__=="__main__":
     Lista = [27,6,-50,21,-3,14,16,-8,42,33,-21,9]
     sumaMayor(Lista)
+    print("Hola")
     
     
