8ef4c45a6109f73d65c4cf7574a263e8524451aa
Mik3Monzon <mmonzonjr@gmail.com>
2021-09-16 23:30:10 -0500

    Tarea 3

---
+++ Tarea3.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 16:42:09 2021
+
+@author: Mike
+"""
+
+def maxSuma(lista):
+    maximo = 0
+    tabla=[0]*len(lista)
+    for i in range(len(lista)):
+        tabla[i] = [0]*len(lista)
+    for i in range(len(lista)):
+        for  j in range(0,i):
+            tabla[i][j] = tabla[i-1][j]+lista[i]
+            if(tabla[i][j] > maximo):
+                maximo = tabla[i][j]
+        tabla[i][i] = lista[i]
+        if(tabla[i][i] > maximo):
+            maximo = tabla[i][i]
+    return (maximo)
+
+def maxSuma2(lista):
+    maximo = 0
+    suma = 0
+    for i in range(len(lista)):
+        if(suma+lista[i] > 0):
+            suma = suma+lista[i]
+        else:
+            suma = 0
+        if(suma > maximo):
+            maximo = suma
+    return (maximo)
+            
+
+if __name__=="__main__":
+    datos = [27,6,-50,21,-3,14,16,-8,42,33,-21,9]
+    print(maxSuma(datos))
+    print(maxSuma2(datos))
