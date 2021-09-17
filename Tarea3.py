91a26937165f9ab9bd58b4b76fe6a376f41db0e5
Mik3Monzon <mmonzonjr@gmail.com>
2021-09-17 11:30:50 -0500

    Tarea 3 Update

--- Tarea3.py
+++ Tarea3.py
@@ -7,6 +7,8 @@ Created on Thu Sep 16 16:42:09 2021
 
 def maxSuma(lista):
     maximo = 0
+    indiceUno = 0
+    indiceDos = 0
     tabla=[0]*len(lista)
     for i in range(len(lista)):
         tabla[i] = [0]*len(lista)
@@ -15,10 +17,12 @@ def maxSuma(lista):
             tabla[i][j] = tabla[i-1][j]+lista[i]
             if(tabla[i][j] > maximo):
                 maximo = tabla[i][j]
+                indiceUno = i
+                indiceDos = j
         tabla[i][i] = lista[i]
         if(tabla[i][i] > maximo):
             maximo = tabla[i][i]
-    return (maximo)
+    return (maximo,indiceUno,indiceDos)
 
 def maxSuma2(lista):
     maximo = 0
@@ -35,5 +39,9 @@ def maxSuma2(lista):
 
 if __name__=="__main__":
     datos = [27,6,-50,21,-3,14,16,-8,42,33,-21,9]
-    print(maxSuma(datos))
+    maximo,rango1,rango2=maxSuma(datos)
+    print("Primera Forma")
+    print(maximo)
+    print(datos[rango2:rango1+1])
+    print("Sgunda Forma")
     print(maxSuma2(datos))
