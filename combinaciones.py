fd899bb4784bb7a41417456f9d374c22861e5a27
Mik3Monzon <mmonzonjr@gmail.com>
2021-09-20 12:10:50 -0500

    Clase 20/09/2021

---
+++ combinaciones.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 12:09:47 2021
+
+@author: Mike
+"""
+
+if __name__=="__main__":
--- permutaciones.py
+++ permutaciones.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 19 20:33:19 2021
+Created on Mon Sep 20 12:08:39 2021
 
 @author: Mike
 """
-
 
 
 if __name__=="__main__":
