c933f96527e2f0b5a4051c87dda5d2723df1abbc
Mik3Monzon <mmonzonjr@gmail.com>
2021-10-09 17:47:51 -0500

    Proyecto Primer Parcial

---
+++ algoritmosOrdenamiento/graficasMetodos.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 17:46:41 2021
+
+@author: Mike
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__=="__main__":
+    datos = pd.read_csv("m1.csv",sep=";")
+    datos2 = pd.read_csv("m2.csv",sep=";")
+    datos4 = pd.read_csv("m4.csv",sep=";")
+    x = datos.N
+    y = datos.Tiempo
+    y2 = datos2.Tiempo
+    y4 = datos4.Tiempo
+    plt.plot(x,y,x,y2,x,y4)
+    plt.xlabel("N")
+    plt.ylabel("Tiempo")
+    plt.title("Burbuja vs Selección vs Inserción vs QuickSort")
+    plt.legend(("Burbuja", "Selección", "Inserción", "QuickSort"), prop = {"size":10}, loc="upper left")
+    plt.grid()
+    plt.show()
