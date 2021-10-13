d63cbc499a1442a491b2362721b406b1fb6afeed
Mik3Monzon <mmonzonjr@gmail.com>
2021-10-13 14:11:04 -0500

    Tarea 5 Update

--- Tarea5.py
+++ Tarea5.py
@@ -11,7 +11,17 @@ def sumaDigitos(numero):
         suma += int(digito)
     return suma
 
-def sumaDigitos2(numero):
+def sumaDigitos2(Lista):
+    if len(Lista) == 0:
+        return 0 
+    else:
+        return int(Lista[0]) + sumaDigitos2(Lista[1:])
 
 if __name__=="__main__":
+    Lista = "456"
+    print("Iterativo")
     print(sumaDigitos(123456789))
+    print("Recursivo")
+    print(sumaDigitos2(Lista))
+    
+    
