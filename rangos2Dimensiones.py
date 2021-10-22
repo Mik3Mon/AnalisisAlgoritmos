fc444e94dab0e01ab55ab82b33aeb049e519885c
Mik3Monzon <mmonzonjr@gmail.com>
2021-10-22 00:39:45 -0500

    Update

--- rangos2Dimensiones.py
+++ rangos2Dimensiones.py
@@ -4,9 +4,29 @@ Created on Mon Oct 18 01:04:24 2021
 
 @author: Mike
 """
-def rangos(Lista):
+import metodosOrdenamiento
+
+def encontrar_rango(central,fin):
+    rango=0
+    rango_total=0
+    for i in range(central,len(lista)):
+        j=i
+        while(j >= 0):
+            if lista[i][1:]>lista[j][1:]:
+                rango+=1
+            j-=1
+    return rango
+
+
     
     
 
 if __name__=="__main__":
-    Lista = [(2,5),(8,4),(10,3),(11,5),(16,7),(19,8),(20,10),(20,15),(21,9),(25,10)]
+    lista = [[21, 1], [7, 26], [6, 24], [25, 5], [14, 25], [9, 15], [23, 2], [3, 11], [1, 10], [7, 12]]
+    central = int(len(lista)/2)
+    fin = int(len(lista))
+    print(central)
+    print(lista)
+    metodosOrdenamiento.metodo_quicksort(lista)
+    print(lista)
+    print('El rango del conjunto de punto B es: ' + str(encontrar_rango(central,fin)))
